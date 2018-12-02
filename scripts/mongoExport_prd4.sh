8bc33a3090ca526d4a78604170ddbb5d0dccf795
dev <gerrit_user1@sina.com>
2018-12-02 23:11:19 +0800

    update mongoExport_prd4
    Change-Id: Ib60757246ca9dbf14a765b0a1535771ff8b4722f

--- scripts/mongoExport_prd4.sh
+++ scripts/mongoExport_prd4.sh
@@ -1,6 +1,6 @@
 scriptDir=/home/dev/Repository/news/scripts
 tmpDir=/home/Data
 ifengSyncDir=/home/dev/Data/rsyncData/prd4/ifeng
-mongoexport -d 'ifeng_content' -c 'contentInfo' --csv -f join_number,comment_number,url,title,author_name,time,id -o ${tmpDir}/ifeng_content_tmp.csv
+mongoexport -d 'ifeng' -c 'contentInfo' --csv -f join_number,comment_number,url,title,author_name,time,id -o ${tmpDir}/ifeng_content_tmp.csv
 mv ${tmpDir}/ifeng_content_tmp.csv ${ifengSyncDir}/ifeng_content.csv
 chmod -R 777 ${ifengSyncDir}/ifeng_content.csv
