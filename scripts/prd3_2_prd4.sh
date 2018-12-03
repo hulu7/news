fea0ea124b9437b4b9826d5a9a97355cd1881f2a
dev <gerrit_user1@sina.com>
2018-12-03 08:32:54 +0800

    update prd3 2 prd4
    Change-Id: I191e14584c3aa223b993fe50b42d4f9aecb30b53

--- scripts/prd3_2_prd4.sh
+++ scripts/prd3_2_prd4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set timeout 100
 set password 'thebestornothing'
-ipFile=/home/dev/Data/rsyncData/prd3/ifeng/ifeng_urls.csv
-toDir=/home/dev/Data/rsyncData/prd3/ifeng/ifeng_urls.csv
+fromDir=/home/dev/Data/rsyncData/prd3
+toDir=/home/dev/Data/rsyncData/
 expect -c "
-  spawn scp ${ipFile} root@prd4:${toDir}
+  spawn scp -r ${fromDir} root@prd4:${toDir}
   expect {
     \"*assword\" {set timeout 30; send \"thebestornothing\r\";}
     \"yes/no\" {send \"yes\r\"; exp_continue;}
