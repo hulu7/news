02dc608f2d02f6fe4e772e226422c38e2ef8198c
dev <gerrit_user1@sina.com>
2018-12-24 22:13:40 +0800

    add timeout for prd3 2 prd4
    Change-Id: I1808214845a16b3686202ccc2c37589409138d91

--- scripts/prd3_2_prd4.sh
+++ scripts/prd3_2_prd4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set timeout 100
+set timeout 600
 set password 'thebestornothing'
 fromDir=/home/dev/Data/rsyncData/prd3
 toDir=/home/dev/Data/rsyncData/
@@ -7,7 +7,7 @@ chmod -R 777 ${fromDir}
 expect -c "
   spawn scp -r ${fromDir} root@prd4:${toDir}
   expect {
-    \"*assword\" {set timeout 30; send \"thebestornothing\r\";}
+    \"*assword\" {set timeout 600; send \"thebestornothing\r\";}
     \"yes/no\" {send \"yes\r\"; exp_continue;}
   }
   expect eof"
