110fc190d0befeba5ad79fb72599d8ceb29806ad
dev <gerrit_user1@sina.com>
2019-01-20 15:24:07 +0800

    update prd3 to prd4 prd4 receive folder
    Change-Id: I20affc23d6a07cbb9d2db3435978a0a69e6e67de

--- scripts/prd3_2_prd4.sh
+++ scripts/prd3_2_prd4.sh
@@ -2,7 +2,7 @@
 set timeout 600
 set password 'thebestornothing'
 fromDir=/home/dev/Data/rsyncData/prd3/sites
-toDir=/home/dev/Data/rsyncData/
+toDir=/home/dev/Data/rsyncData/prd3
 chmod -R 777 ${fromDir}
 expect -c "
   spawn scp -r ${fromDir} root@prd4:${toDir}
