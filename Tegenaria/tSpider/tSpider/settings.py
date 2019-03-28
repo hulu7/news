b47a7cc474504ae110405f4e2991982add5a14af
dev <gerrit_user1@sina.com>
2019-03-29 07:15:22 +0800

    add baijia
    Change-Id: I342ae87fe0eb8148f1f5fb5a74db424cc0c5e08a

--- Tegenaria/tSpider/tSpider/settings.py
+++ Tegenaria/tSpider/tSpider/settings.py
@@ -107,3 +107,4 @@ class Settings():
         self.YICAI = self.SettingsFormat('yicai', '第一财经', 30, 2, False)
         self.WALLSTREETCN = self.SettingsFormat('wallstreetcn', '华尔街见闻', 30, 2, False)
         self.I36KR = self.SettingsFormat('36kr', '36氪', 30, 2, False)
+        self.BAIJIA = self.SettingsFormat('baijia', '百家号', 60, 2, False)
