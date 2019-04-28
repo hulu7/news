bd7d2aa06e48990541dfab28981c4bd906d06a84
dev <gerrit_user1@sina.com>
2019-04-29 07:13:39 +0800

    fix weixin_talk
    Change-Id: I93f30cf1feaed20292b45dc988727a52e9708274

--- Tegenaria/tSpider/tSpider/restart/temp_2_webserver0.sh
+++ Tegenaria/tSpider/tSpider/restart/temp_2_webserver0.sh
@@ -4,11 +4,7 @@ LOG_DATE=$(date "+%Y-%m-%d")
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 SALTICIDAEPATH=/home/dev/Repository/news/Tegenaria/tSpider/tSpider/talk
 LOGPATH=/home/dev/Data/rsyncData/prd4/log
-sm=`ps -fe |grep "weixin_talk.py" |grep -v "grep" |wc -l`
-if [ $sm -eq 0 ]; then
-     echo "${TIME}: Start to upload html and image files ..." >> ${LOGPATH}/${LOG_DATE}_log.log
-     cd ${SALTICIDAEPATH}
-     python weixin_talk.py
-else
-  echo "weixin_talk started"
-fi
+ps -ef | grep "weixin_talk.py" |grep -v grep| cut -c 9-15 | xargs kill -9
+echo "${TIME}: Start to upload html and image files ..." >> ${LOGPATH}/${LOG_DATE}_log.log
+cd ${SALTICIDAEPATH}
+python weixin_talk.py
