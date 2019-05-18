d3c707ee4650b61c10727170a6b9e18af073d232
dev <gerrit_user1@sina.com>
2019-05-18 16:54:01 +0800

    update restart and shutdown os
    Change-Id: I2e6b98872d60418f9ff2bb0a1c6513cc4560eea2

--- scripts/shutdown_os.sh
+++ scripts/shutdown_os.sh
@@ -2,7 +2,7 @@ DATE=$(date "+%Y-%m-%d")
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 scriptDir=/home/dev/Data/rsyncData/prd4/log
 echo "${TIME}: shutdown system"  >> ${scriptDir}/${DATE}_log.log
-ps aux | grep "mongoRestart_prd3.sh" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+ps aux | grep "mongoRestart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
 ps aux | grep "python" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
 redis-cli -h 127.0.0.1 -p 6379 shutdown >> ${scriptDir}/${DATE}_log.log
 service mongod stop  >> ${scriptDir}/${DATE}_log.log
