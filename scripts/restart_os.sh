10d12c4367ae161506f629797e5e8dd97d73496b
dev <gerrit_user1@sina.com>
2019-05-18 17:09:33 +0800

    update restart and shutdown
    Change-Id: I0c7b69975375d6922121649c1042a520e012ab4a

--- scripts/restart_os.sh
+++ scripts/restart_os.sh
@@ -2,8 +2,11 @@ DATE=$(date "+%Y-%m-%d")
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 scriptDir=/home/dev/Data/rsyncData/prd4/log
 echo "${TIME}: restart system"  >> ${scriptDir}/${DATE}_log.log
-ps aux | grep "mongoRestart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+ps aux | grep "camel_restart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+ps aux | grep "tegenaria_restart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
 ps aux | grep "python" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+ps aux | grep "mongoRestart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+ps aux | grep "redis_restart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
 redis-cli -h 127.0.0.1 -p 6379 shutdown >> ${scriptDir}/${DATE}_log.log
 service mongod stop >> ${scriptDir}/${DATE}_log.log
 reboot >> ${scriptDir}/${DATE}_log.log
