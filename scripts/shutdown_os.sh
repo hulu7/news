b9f912e3238fb007f00350257ee068b84c5e6650
dev <gerrit_user1@sina.com>
2019-05-21 20:16:35 +0800

    refactor redis restart and shutdown os restart os
    Change-Id: Ie0f5e0c7052031e8bd70dc1f5c8f7bfb9e5c4c08

--- scripts/shutdown_os.sh
+++ scripts/shutdown_os.sh
@@ -1,13 +1,24 @@
 DATE=$(date "+%Y-%m-%d")
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 scriptDir=/home/dev/Data/rsyncData/prd4/log
+echo "${TIME}: shutdown system"
 echo "${TIME}: shutdown system"  >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: kill camel_restart"
 ps aux | grep "camel_restart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: kill tegenaria_restart"
 ps aux | grep "tegenaria_restart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: kill python"
 ps aux | grep "python" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: kill mongoRestart"
 ps aux | grep "mongoRestart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: kill redis_restart"
 ps aux | grep "redis_restart" |grep -v grep| cut -c 9-15 | xargs kill -9 >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: stop redis"
 redis-cli -h 127.0.0.1 -p 6379 shutdown >> ${scriptDir}/${DATE}_log.log
-service mongod stop  >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: stop mongodb"
+service mongod stop >> ${scriptDir}/${DATE}_log.log
 echo 'y' | rm /home/dev/Data/mongodb/data/mongod.lock >> ${scriptDir}/${DATE}_log.log
+echo "${TIME}: rm dump.rdb"
+cd ~
+echo 'y' | rm dump.rdb >> ${scriptDir}/${DATE}_log.log
 shutdown >> ${scriptDir}/${DATE}_log.log
