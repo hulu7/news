802c1c3d013896024ff15a910c9bdfc3403b045b
dev <gerrit_user1@sina.com>
2019-05-25 08:35:17 +0800

    update redis restart
    Change-Id: I4d4d15662529d48fa8bb935fa90c3963c037c444

--- scripts/redis_restart.sh
+++ scripts/redis_restart.sh
@@ -2,8 +2,6 @@ export PATH=/root/perl5/bin:/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin
 sm=`ps -fe |grep "*:6379" |grep -v "grep" |wc -l`
 if [ $sm -eq 0 ]; then
    echo "start redis"
-   cd ~
-   echo 'y' | rm dump.rdb
    redis-server &
 else
    echo "redis started"
