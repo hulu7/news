6eea20931d411fe3226dd0657586d7ac53117f25
dev <gerrit_user1@sina.com>
2020-01-17 20:24:23 +0800

    add path for camelBone
    Change-Id: Ib4e5df32252512cdd7da34c99ff7d6263a382c6c

--- Tegenaria/tSpider/tSpider/middlewares/camelBone.py
+++ Tegenaria/tSpider/tSpider/middlewares/camelBone.py
@@ -6,6 +6,7 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+sys.path.append("/home/dev/Repository/news/")
 from lxml import etree
 import gc
 from Tegenaria.tSpider.tSpider.browserRequest import BrowserRequest
