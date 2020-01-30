6184ae64073442873511e208da54ed7885673d7c
dev <gerrit_user1@sina.com>
2020-01-30 21:24:40 +0800

    update production merge
    Change-Id: I0d13dcc64dafeebb6b59b4e3de01d1c72e09f1f6

--- scripts/production_merge.sh
+++ scripts/production_merge.sh
@@ -2,5 +2,5 @@ DATE=$(date "+%Y-%m-%d")
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 LOG=/home/dev/Data/rsyncData/prd4/log
 SCRIPTS=/home/dev/Repository/news/scripts
-echo "${TIME}: merge production start"  >> ${LOG}/${DATE}_log.log
-python ${SCRIPTS}/production_merge.py  > ${LOG}/${DATE}_log.log
+echo "${TIME}: merge production start" >> ${LOG}/${DATE}_log.log
+python ${SCRIPTS}/production_merge.py
--- scripts/production_merge_and_update.sh
+++ scripts/production_merge_and_update.sh
@@ -3,6 +3,6 @@ TIME=$(date "+%Y-%m-%d %H:%M:%S")
 LOG=/home/dev/Data/rsyncData/prd4/log
 SCRIPTS=/home/dev/Repository/news/scripts
 echo "${TIME}: merge production start"  >> ${LOG}/${DATE}_log.log
-python ${SCRIPTS}/production_merge.py  > ${LOG}/${DATE}_log.log
-echo "${TIME}: start to update deepinews"  >> ${LOG}/${DATE}_log.log
+python ${SCRIPTS}/production_merge.py  >> ${LOG}/${DATE}_log.log
+echo "${TIME}: start to update deepinews" >> ${LOG}/${DATE}_log.log
 python ${SCRIPTS}/update_mongo_deepinews.py >> ${LOG}/${DATE}_log.log
