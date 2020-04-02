d6e64fea344cb96ba4fadb85d2014a713be66f09
dev <gerrit_user1@sina.com>
2020-04-02 22:27:12 +0800

    update crontab for prd4 and webserver0
    Change-Id: I3867b9b9118c036417fe8ea80141e681e6f668f6

--- scripts/crontab_e_prd4.sh
+++ scripts/crontab_e_prd4.sh
@@ -4,15 +4,10 @@
 */5 * * * * sh /home/dev/Repository/news/scripts/mongoExport_prd4.sh
 */1 * * * * sh /home/dev/Repository/news/scripts/mongoRestart_prd4.sh
 */1 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/tegenaria_restart.sh
-*/1 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/sshUpload_restart.sh
-*/1 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/salticidae_restart.sh
-*/1 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/restart_golden.sh
-*/1 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/temp_2_webserver0.sh
+*/5 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/sshUpload_restart.sh
 */15 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/monitor_restart.sh
-*/30 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/Chronus/chrous_restart.sh
 */5 * * * * sh /home/dev/Repository/news/supplier/classification/start_classify.sh
 */5 * * * * sh /home/dev/Repository/news/supplier/commit/start_commit.sh
-00 07 * * * sh /home/dev/Repository/news/supplier/send/start_send.sh
 */7 * * * * sh /home/dev/Repository/news/scripts/production_merge.sh
 */13 * * * * sh /home/dev/Repository/news/scripts/prd4_2_webserver0.sh
 */5 * * * * sh /home/dev/Repository/news/scripts/redis_restart.sh
--- scripts/crontab_e_webserver0.sh
+++ scripts/crontab_e_webserver0.sh
@@ -2,6 +2,7 @@
 */1 * * * * sh /home/dev/Repository/news/scripts/deepnews_server_restart.sh
 */1 * * * * sh /home/dev/Repository/news/scripts/update_mongo_deepinews.sh
 */1 * * * * sh /home/dev/Repository/news/scripts/update_monitor.sh
+*/1 * * * * sh /home/dev/Repository/news/scripts/update_html.sh
 */1 * * * * sh /home/dev/Repository/news/scripts/redis_restart.sh
 */1 * * * * sh /home/dev/Repository/news/scripts/adminMongo_restart.sh
 */1 * * * * sh /home/dev/Repository/news/scripts/mongoRestart_prd3.sh
