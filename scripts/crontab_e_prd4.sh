f66f8e25b115f90ad6c0cc5b4608411255ad0ef4
dev <gerrit_user1@sina.com>
2020-04-05 00:04:31 +0800

    update crontab e for prd4
    Change-Id: Id608e420e85fccfc911f5cff274d8cc64308c251

--- scripts/crontab_e_prd4.sh
+++ scripts/crontab_e_prd4.sh
@@ -9,6 +9,6 @@
 */5 * * * * sh /home/dev/Repository/news/supplier/classification/start_classify.sh
 */5 * * * * sh /home/dev/Repository/news/supplier/commit/start_commit.sh
 */7 * * * * sh /home/dev/Repository/news/scripts/production_merge.sh
-*/13 * * * * sh /home/dev/Repository/news/scripts/prd4_2_webserver0.sh
+*/10 * * * * sh /home/dev/Repository/news/Tegenaria/tSpider/tSpider/restart/upload_mongo_data_restart.sh
 */5 * * * * sh /home/dev/Repository/news/scripts/redis_restart.sh
 */120 * * * * sh /home/dev/Repository/news/scripts/backup/run.sh /home/dev/Repository/news/scripts/backup/prd4.txt
